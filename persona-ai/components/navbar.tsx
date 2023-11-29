c3e0b09bb733e895b9a3bfb0362b9222aa44b2ff
yslee96 <dbstn322@gmail.com>
2023-11-29 17:11:12 +0900

    created root page and navbar

---
+++ persona-ai/components/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { Menu, Sparkle } from "lucide-react";
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/componentsg/mode-toggle";
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+});
+
+const Navbar = () => {
+    return ( 
+        <div className="fixed w-full z-50 flex justify-between
+        items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+            <div className="flex items-center">
+                <Menu className="block md:hidden"/>
+                <Link href="/">
+                    <h1 className={cn(
+                        "hidden md:block text-xl md:text-3xl font-bold text-primary",
+                        font.className
+                        )}>
+                        persona.ai 
+                    </h1>
+                </Link>
+            </div>
+            <div className="flex items-center gap-x-3">
+                <Button size="sm">
+                    Upgrade
+                    <Sparkle className="h-4 w-4 fill-white text-white ml-2" />
+                </Button>
+                <ModeToggle />
+            </div>
+        </div>
+
+    );
+}
+
+export default Navbar;
